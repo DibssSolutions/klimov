2b8d08aebab8ddb51255024288d5dc0af6c6f582
KopylovVitalik <vitalik.kopylov@gmail.com>
2018-07-18 19:03:56 +0300

    JSON

--- src/js/app.js
+++ src/js/app.js
@@ -1,1 +1,15 @@
 import './_common';
+import DoSlide from 'do-slide';
+import { runInNewContext } from 'vm';
+
+new DoSlide('.ds-container', {
+//   transitionInClass    : 'slide-in',
+  //   transitionOutClass   : 'slide-out',
+  translate3d          : false,
+  duration: 1, 
+  minInterval          : 250
+//   onBeforeChange(function(curIndex, tarIndex, cur, tar) {
+//     this.classList.add('slide-out-2');
+//     setTimeout(runInNewContext, 500);
+//   }
+});
