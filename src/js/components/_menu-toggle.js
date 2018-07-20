c1a55d8cdbfdc02fee79c50228c8b5e192db29e0
KopylovVitalik <vitalik.kopylov@gmail.com>
2018-07-20 14:49:31 +0300

    main-page

--- src/js/components/_menu-toggle.js
+++ src/js/components/_menu-toggle.js
@@ -4,6 +4,9 @@ $('.menu-trigger').click(function() {
   if (open) {
     $('.menu-trigger').removeClass('menu-is-open');
     $('.menu').fadeOut(500);
+    $('.top-nav .col-31').fadeIn();
+    $('.top-nav .col-45').fadeIn();
+    $('.logo').removeClass('logo-grey');
     open = false;
   } else {
     $('.menu-trigger').addClass('menu-is-open');
@@ -12,6 +15,7 @@ $('.menu-trigger').click(function() {
       .fadeIn(500);
     $('.top-nav .col-31').fadeOut();
     $('.top-nav .col-45').fadeOut();
+    $('.logo').toggleClass('logo-grey');
     open = true;
   }
 });
