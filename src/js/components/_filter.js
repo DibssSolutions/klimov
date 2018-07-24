6cdb0247a56c6864d88a50d43bf5a571df874c06
KopylovVitalik <vitalik.kopylov@gmail.com>
2018-07-25 00:49:45 +0300

    filter popup

--- src/js/components/_filter.js
+++ src/js/components/_filter.js
@@ -1,17 +1,23 @@
 /* eslint-disable */
-var Isotope = require("isotope-layout");
+var Isotope = require('isotope-layout');
 
-var filter = document.getElementById("filter");
+var filter = document.getElementById('filter');
 
 if (filter !== null) {
-  var iso = new Isotope(".filter__layout", {
-    itemSelector: ".filter__item",
-    layoutMode: "fitRows",
-    transitionDuration: "0"
+  $('.filter__label').click(() => {
+    $('.filter__filter').addClass('is-active');
   });
 
-  $(".filter__btn").click(function() {
-    var filterValue = $(this).attr("data-filter");
+  var iso = new Isotope('.filter__layout', {
+    itemSelector: '.filter__item',
+    layoutMode: 'fitRows',
+    transitionDuration: '0'
+  });
+
+  $('.filter__btn').click(function() {
+    var filterValue = $(this).attr('data-filter');
     iso.arrange({ filter: filterValue });
+    $('.js-filter-category-selected').text($(this).text());
+    $('.filter__filter').removeClass('is-active');
   });
 }
