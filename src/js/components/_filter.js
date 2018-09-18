7c36f777cd48858482d7655fd7496b012a85cae5
Валерий <kaktuz34@gmail.com>
2018-09-18 20:39:46 +0300

    added post

--- src/js/components/_filter.js
+++ src/js/components/_filter.js
@@ -53,14 +53,39 @@ if (filter) {
     $(this).removeClass('is-active');
   })
 }
+
+function load_more(offset){
+  const data = 'offset=' + offset + '&action=more_post_ajax';
+  const admin_ajax = $('#filter').data('ajax-url');
+  $.ajax({
+    type: "POST",
+    dataType: "html",
+    url: admin_ajax,
+    data,
+    success: function(data){
+      var $data = $(data);
+      if($data.length){
+        $(".filter__layout").append($data);
+        $(".loader").hide();
+        ($('[data-filter]').hasClass('is-active')) 
+          ? $('[data-filter].is-active').trigger('click')
+          : $('[data-filter="*"]').trigger('click');
+      }
+    }
+  });
+  return false;
+}
 $(window).on('scroll', function(){
   if (!$('#filter').length) return;
   let offsetTop = $(window).scrollTop();
   let filterOffset = $('.filter__inner').offset().top;
   let docHeigth = $('.filter__inner').outerHeight();
-  let winHeight = $(window).outerHeight()
-  if (offsetTop + winHeight >= filterOffset + docHeigth) {
+  let winHeight = $(window).outerHeight();
+  let imageLength = $('#filter img').length - 1 ;
 
+  if (offsetTop + winHeight >= filterOffset + docHeigth) {
+    $(".loader").show();
+    load_more(imageLength)
   }
   
 })
