42a8071f7517fc5df1cf2a90ccb41c15db035454
Валерий <kaktuz34@gmail.com>
2019-01-31 12:07:42 +0200

    update trigger scroll

--- src/js/components/_animation-scroll.js
+++ src/js/components/_animation-scroll.js
@@ -1,7 +1,8 @@
 import ScrollReveal from 'scrollreveal';
+import { DOC } from '../_constants';
 
 window.sr = ScrollReveal();
-
+DOC.ready(() => { window.scrollTo(window.scrollX, window.scrollY + 3); });
 var scrollOptions = {
   scale: 1,
   duration: 1500,
